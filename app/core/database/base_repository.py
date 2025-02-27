3e9958c67da21d783c8d389a1d1e522b4572c289
Maxvel <velichko.m@hub.sknt.ru>
2025-02-27 15:58:54 +0300

    научился проставлять типизацию

--- app/core/database/base_repository.py
+++ app/core/database/base_repository.py
@@ -3,7 +3,7 @@ from .connector import connector
 
 class Base_repository:
     def __init__(self, conn: connector):
-        self.connector = conn
+        self.connector: connector = conn
 
     def query(self, query: str, values: dict):
         self.connector.query(query, values)
